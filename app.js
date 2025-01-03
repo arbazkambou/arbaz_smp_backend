fe305898065de56c52b8eeab190a2647f2af8bca
Arbaz Shoukat <arbazkamboh342@.com>
2025-01-03 23:28:14 +0500

    fix api url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://arbaz-smp-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     credentials: true,
   })
