0b30e5e87f9cd14867aa7f12535ffafb22c85c05
Arbaz Shoukat <arbazkamboh342@.com>
2025-01-04 03:23:08 +0500

    fix

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -119,8 +119,8 @@ export async function login(req, res, next) {
 
     const cookieOptions = {
       maxAge: process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-      secure: true, //So that it can  be sent on https connection
-      sameSite: "none",
+      secure: false, //So that it can  be sent on https connection
+      // sameSite: "none",
       // secure: process.env.NODE_ENV === "production", //So that it can  be sent on https connection
       httpOnly: true, //So that browser can not modified it
     };
